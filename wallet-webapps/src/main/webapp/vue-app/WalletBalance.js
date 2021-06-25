9af20ec78083a4e57cacab9b498bab606f0e6513
mohamed <mrdidi@exoplatform.com>
2021-06-25 18:38:51 +0100

    add initComponents in files js

--- wallet-webapps/src/main/webapp/vue-app/WalletBalance.js
+++ wallet-webapps/src/main/webapp/vue-app/WalletBalance.js
@@ -14,6 +14,8 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+import './initComponents.js';
+
 Vue.use(Vuetify);
 
 const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
