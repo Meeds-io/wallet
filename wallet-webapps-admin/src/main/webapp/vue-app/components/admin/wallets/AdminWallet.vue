e04e99a98a374d9ff15092b59aaa82d866da972c
GouadriaHanen <hgouadria@exoplatform.com>
2021-07-09 15:44:59 +0100

    symbol size

--- wallet-webapps-admin/src/main/webapp/vue-app/components/admin/wallets/AdminWallet.vue
+++ wallet-webapps-admin/src/main/webapp/vue-app/components/admin/wallets/AdminWallet.vue
@@ -35,7 +35,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
         indeterminate
         size="20" />
       <template v-else>
-        <span class="symbol"> {{ tokenSymbol }} </span> <span class="fundsLabels"> {{ tokenBalance }} </span>
+        <span class="symbol fundsLabels"> {{ tokenSymbol }} </span> <span class="fundsLabels"> {{ tokenBalance }} </span>
         <v-icon
           v-if="adminBalanceTooLow"
           color="orange"
