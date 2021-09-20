4dfc3d6dbafd83a0fb433ccf00b5e577db9cf0a3
mzorai <mzorai@exoplatform.com>
2021-09-20 12:52:53 +0100

    Task-48905 : Refactor UserPopup  and SpacePopup Labels (#105)
    Before this fix, space popup labels are duplicated in a lot of properties files
    This fix modify everywhere spacePopup is called to not use duplicated labels.
    Labels which are no more necessary are removed
    
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>

--- wallet-webapps-common/src/main/webapp/vue-app/components/ProfileChip.vue
+++ wallet-webapps-common/src/main/webapp/vue-app/components/ProfileChip.vue
@@ -177,19 +177,6 @@ export default {
     };
   },
   computed: {
-    labels() {
-      return {
-        CancelRequest: this.$t('exoplatform.wallet.label.profile.CancelRequest'),
-        Confirm: this.$t('exoplatform.wallet.label.profile.Confirm'),
-        Connect: this.$t('exoplatform.wallet.label.profile.Connect'),
-        Ignore: this.$t('exoplatform.wallet.label.profile.Ignore'),
-        RemoveConnection: this.$t('exoplatform.wallet.label.profile.RemoveConnection'),
-        StatusTitle: this.$t('exoplatform.wallet.label.profile.StatusTitle'),
-        join: this.$t('exoplatform.wallet.label.profile.join'),
-        leave: this.$t('exoplatform.wallet.label.profile.leave'),
-        members: this.$t('exoplatform.wallet.label.profile.members'),
-      };
-    },
     url() {
       if (!this.profileType || this.profileType === 'user') {
         return `${eXo.env.portal.context}/${eXo.env.portal.portalName}/profile/${this.profileId}`;
@@ -226,7 +213,6 @@ export default {
             membershipRestUrl: '/portal/rest/v1/social/spacesMemberships?space={0}&returnSize=true',
             defaultAvatarUrl: this.avatar ? this.avatar : `/portal/rest/v1/social/spaces/${this.profileId}/avatar`,
             deleteMembershipRestUrl: '/portal/rest/v1/social/spacesMemberships/{0}:{1}:{2}',
-            labels: this.labels,
             content: false,
             keepAlive: true,
             defaultPosition: this.tiptipPosition || 'left_bottom',
@@ -238,7 +224,6 @@ export default {
           $(`#${this.id}`).userPopup({
             restURL: '/portal/rest/social/people/getPeopleInfo/{0}.json',
             userId: this.profileId,
-            labels: this.labels,
             content: false,
             keepAlive: true,
             defaultPosition: this.tiptipPosition || 'left_bottom',
