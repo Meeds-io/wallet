7e97232c13914c31151e6da1d6476acdcfd9efff
mohamed <mrdidi@exoplatform.com>
2021-10-07 17:33:52 +0100

    Remove message of saving the key

--- wallet-webapps-common/src/main/webapp/vue-app/wallet-settings/components/WalletSettingsDetails.vue
+++ wallet-webapps-common/src/main/webapp/vue-app/wallet-settings/components/WalletSettingsDetails.vue
@@ -80,7 +80,7 @@ export default {
     },
   },
   data: () => ({
-    id:`WalletSettingsDetails${parseInt(Math.random() * 10000)}`,
+    id: `WalletSettingsDetails${parseInt(Math.random() * 10000)}`,
     displayed: true,
     displayManagePasswordDetails: false,
   }),
