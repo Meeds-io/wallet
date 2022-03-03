86bdabad048cc287519aee4244dacd2b314ae66a
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-03-03 16:56:10 +0100

    Task-54919: Hide send meeds in user popover (#196)

--- wallet-webapps/src/main/webapp/vue-app/components/api/WalletAPIApp.vue
+++ wallet-webapps/src/main/webapp/vue-app/components/api/WalletAPIApp.vue
@@ -394,11 +394,6 @@ export default {
       };
       extensionRegistry.registerExtension('profile-extension', 'action', profileExtensionAction);
       document.dispatchEvent(new CustomEvent('profile-extension-updated', { detail: profileExtensionAction}));
-      extensionRegistry.registerComponent('UserPopover', 'user-popover-action', {
-        id: 'wallet',
-        vueComponent: Vue.options.components['popover-wallet-button'],
-        rank: 20,
-      });
     },
   },
 };
