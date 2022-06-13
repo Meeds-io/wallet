836219cb4ba2d2435744f21c6635fda4081ffc9b
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-06-13 16:31:53 +0100

    Update SendTokensForm.vue
    width omitted from exo-drawer

--- wallet-webapps-common/src/main/webapp/vue-app/components/SendTokensForm.vue
+++ wallet-webapps-common/src/main/webapp/vue-app/components/SendTokensForm.vue
@@ -17,7 +17,6 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 <template>
   <exo-drawer
     ref="sendTokensForm"
-    width="100%"
     :right="!$vuetify.rtl">
     <template slot="title">
       <div>
