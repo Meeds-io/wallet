9994097226affae2b7069e384e49b9fde289fbc3
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2022-11-18 16:45:38 +0100

    feat: Overview Page Add wallet block in rewards portlet - MEED-936 - Meeds-io/MIPs#10 (#316)
    This change will add the wallet block in the rewards portlet.

---
+++ wallet-webapps-common/src/main/webapp/vue-app/wallet-user-balance/components/WalletUserBalance.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-card flat>
+      <v-card-text class="pa-0 d-flex justify-center flex-nowrap text-color display-1 font-weight-bold big-number">
+        <span class="my-2 tertiary-color">{{ symbol }}</span>
+        <span class="text-truncate ma-2 display-1 font-weight-bold">{{ balanceToDisplay }}</span>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</template>
+<script>
+export default {
+  data: () => ({
+    wallet: null,
+    contractDetails: null,
+  }),
+  computed: {
+    symbol() {
+      return this.contractDetails?.symbol;
+    },
+    balance() {
+      return this.wallet?.tokenBalance;
+    },
+    balanceToDisplay() {
+      return this.balance?.toFixed(2);
+    }
+  },
+  created() {
+    this.walletUtils.initSettings(false, true, true)
+      .then(() => {
+        this.wallet = Object.assign({}, window.walletSettings.wallet);
+        this.contractDetails = Object.assign({},window.walletSettings.contractDetail); 
+      });
+  }
+};
+</script>
