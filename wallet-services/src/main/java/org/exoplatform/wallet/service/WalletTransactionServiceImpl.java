d89cf9d36cc7f83183434cfb1e72ac326eda3e34
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-12-22 19:30:14 +0100

    feat: Cleanup Dependencies to rely on Spring Dependencies Tree - Meeds-io/MIPs#57

--- wallet-services/src/main/java/org/exoplatform/wallet/service/WalletTransactionServiceImpl.java
+++ wallet-services/src/main/java/org/exoplatform/wallet/service/WalletTransactionServiceImpl.java
@@ -25,7 +25,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.container.xml.InitParams;
