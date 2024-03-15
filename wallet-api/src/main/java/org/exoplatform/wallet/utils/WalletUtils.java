bf61c07c5c69b438d00d7f3f162a601ee98f957f
Romain Dénarié <romain.denarie@exoplatform.com>
2024-03-15 10:27:00 +0100

    fix: When a transaction is boosted, the reference of this tx in reward dashboard is not updated - EXO-70361
    Before this fix, when a tx replace another one, the broadcastTransactionReplacedEvent is correctly launch,
    but the first test of this function verify if the newTx is pending. If the new tx is pending, it do nothing.
    
    As the new tx is always pending, this function never do something
    This commit change the test to do nothing when the new tx IS NOT pending.
    
    Resolves meeds-io/meeds#1516

--- wallet-api/src/main/java/org/exoplatform/wallet/utils/WalletUtils.java
+++ wallet-api/src/main/java/org/exoplatform/wallet/utils/WalletUtils.java
@@ -1020,7 +1020,7 @@ public class WalletUtils {
     String oldHash = "";
     String newHash = "";
     try {
-      if (oldTransaction == null || newTransaction == null || oldTransaction.isPending() || newTransaction.isPending()) {
+      if (oldTransaction == null || newTransaction == null || oldTransaction.isPending() || !(newTransaction.isPending())) {
         return;
       }
       oldHash = oldTransaction.getHash();
