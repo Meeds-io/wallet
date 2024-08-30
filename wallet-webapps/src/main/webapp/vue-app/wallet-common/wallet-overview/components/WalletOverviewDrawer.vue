c833faa4e8c8e587557c723006a5d33cafc9bbd4
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-08-30 13:46:49 +0100

    feat: Enhance UX of My Rewards Overview Widget Drawer - MEED-7373 - Meeds-io/MIPs#155 (#548)

--- wallet-webapps/src/main/webapp/vue-app/wallet-common/wallet-overview/components/WalletOverviewDrawer.vue
+++ wallet-webapps/src/main/webapp/vue-app/wallet-common/wallet-overview/components/WalletOverviewDrawer.vue
@@ -17,21 +17,49 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 <template>
   <exo-drawer
     ref="drawer"
+    v-model="drawer"
     :right="!$vuetify.rtl"
     :loading="loading"
-    class="rewardsOverviewDrawer">
+    class="rewardsOverviewDrawer"
+    allow-expand>
     <template slot="title">
-      {{ title }}
+      {{ $t('wallet.overview.rewards.title') }}
     </template>
-    <template slot="content">
-      <v-list v-if="rewardsList">
-        <wallet-overview-reward-item
-          v-for="reward in rewardsList"
-          :key="reward.technicalId"
-          :reward-item="reward"
-          :symbol="symbol"
-          class="border-color border-radius ma-4" />
-      </v-list>
+    <template v-if="drawer" slot="content">
+      <v-tabs
+        v-model="tabName"
+        slider-size="4">
+        <v-tab
+          tab-value="rewards"
+          href="#rewards">
+          {{ $t('wallet.overview.rewards') }}
+        </v-tab>
+        <v-tab
+          tab-value="transactions"
+          href="#transactions">
+          {{ $t('wallet.overview.transactions') }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items
+        v-model="tabName"
+        class="px-4">
+        <v-tab-item value="rewards">
+          <v-list v-if="rewardsList">
+            <wallet-overview-reward-item
+              v-for="reward in rewardsList"
+              :key="reward.technicalId"
+              :reward-item="reward"
+              :symbol="symbol"
+              class="border-color border-radius mb-2" />
+          </v-list>
+        </v-tab-item>
+        <v-tab-item value="transactions">
+          <wallet-reward-transactions-list
+            :wallet="wallet"
+            :contract-details="contract"
+            class="lastTransactionsList overflow-x-hidden mb-2" />
+        </v-tab-item>
+      </v-tabs-items>
     </template>
     <template v-if="hasMore" slot="footer">
       <v-spacer />
@@ -47,37 +75,65 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
     </template>
   </exo-drawer>
 </template>
-
 <script>
 export default {
-  props: {
-    symbol: {
-      type: String,
-      default: 'sent',
-    },
-  },
   data: () => ({
+    drawer: false,
     pageSize: 20,
     limit: 20,
     rewardsList: [],
     loading: false,
+    contract: null,
+    wallet: null,
+    tabName: 'rewards',
   }),
   computed: {
     hasMore() {
       return this.rewardsList.length >= this.limit;
     },
+    symbol() {
+      return this.contract?.symbol;
+    },
+    identity() {
+      return {
+        id: this.wallet?.technicalId,
+        enabled: !this.wallet?.disabledUser,
+        deleted: this.wallet?.deletedUser,       
+        fullName: this.wallet?.name,
+        avatar: this.wallet?.avatar,
+        external: false,
+      };
+    },
+    balanceToDisplay() {
+      return Number.isFinite(Number(this.wallet?.tokenBalance))
+        ? new Intl.NumberFormat(eXo.env.portal.language, {
+          style: 'decimal',
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        }).format(Math.trunc(this.wallet?.tokenBalance))
+        : 0;
+    },
+  },
+  created() {
+    this.$root.$on('wallet-overview-drawer', this.open);
   },
   methods: {
-    reset() {
-      this.limit = 20;
-      this.size = 0;
-      this.rewardsList = [];
-    },
-    open(title) {
-      this.title = title;
+    open(tabName) {
+      this.tabName = tabName || 'rewards';
       this.reset();
       this.retrieveList();
       this.$refs.drawer.open();
+    },
+    reset() {
+      this.limit = 20;
+      this.size = 0;
+      this.wallet = window.walletSettings.wallet
+        && {...window.walletSettings.wallet}
+        || {};
+      this.contract = window.walletSettings.contractDetail
+        && {...window.walletSettings.contractDetail}
+        || {};
+      this.rewardsList = [];
     },
     loadMore() {
       this.limit += this.pageSize;
