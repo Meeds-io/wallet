e2ff28f8831356f6a0a9f383f2425302ca8c1179
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-09-06 10:27:25 +0200

    feat: Manage Wallet from rewards management page - MEED-7380 - Meeds-io#154 (#553)
    This PR will allow admin to Manage Wallet from rewards management page.

--- wallet-webapps/src/main/webapp/vue-app/wallet-reward/initComponents.js
+++ wallet-webapps/src/main/webapp/vue-app/wallet-reward/initComponents.js
@@ -1,4 +1,7 @@
 import RewardApp from './components/RewardApp.vue';
+import RewardManagement from './components/reward/RewardManagement.vue';
+import CurrentBalance from './components/reward/CurrentBalance.vue';
+import BudgetConfiguration from './components/reward/BudgetConfiguration.vue';
 import RewardDetailModal from './components/reward/modal/RewardDetailModal.vue';
 import TimeZoneSelectBox from './components/reward/TimeZoneSelectBox.vue';
 import ConfigurationTab from './components/reward/ConfigurationTab.vue';
@@ -8,6 +11,9 @@ import TeamsListTab from './components/reward/TeamsListTab.vue';
 
 const components = {
   'wallet-reward-app': RewardApp,
+  'wallet-reward-management': RewardManagement,
+  'wallet-current-balance': CurrentBalance,
+  'wallet-budget-configuration': BudgetConfiguration,
   'wallet-reward-reward-detail-modal': RewardDetailModal,
   'wallet-reward-timezone-selectbox': TimeZoneSelectBox,
   'wallet-reward-configuration-tab': ConfigurationTab,
