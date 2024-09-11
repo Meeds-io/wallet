800be2d5f8afd1ede6d4ea564cba2e3389211301
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-09-11 16:09:12 +0200

    feat: Suggest to create a rewarding  budget - MEED-7379 - Meeds-io/MIPs#154 (#554)
    Ability for rewarding admin to create/update reward budget

--- wallet-webapps/src/main/webapp/vue-app/wallet-reward/initComponents.js
+++ wallet-webapps/src/main/webapp/vue-app/wallet-reward/initComponents.js
@@ -2,24 +2,16 @@ import RewardApp from './components/RewardApp.vue';
 import RewardManagement from './components/reward/RewardManagement.vue';
 import CurrentBalance from './components/reward/CurrentBalance.vue';
 import BudgetConfiguration from './components/reward/BudgetConfiguration.vue';
-import RewardDetailModal from './components/reward/modal/RewardDetailModal.vue';
+import BudgetConfigurationDrawer from './components/reward/BudgetConfigurationDrawer.vue';
 import TimeZoneSelectBox from './components/reward/TimeZoneSelectBox.vue';
-import ConfigurationTab from './components/reward/ConfigurationTab.vue';
-import SendRewardsTab from './components/reward/SendRewardsTab.vue';
-import TeamForm from './components/reward/TeamForm.vue';
-import TeamsListTab from './components/reward/TeamsListTab.vue';
 
 const components = {
   'wallet-reward-app': RewardApp,
   'wallet-reward-management': RewardManagement,
   'wallet-current-balance': CurrentBalance,
   'wallet-budget-configuration': BudgetConfiguration,
-  'wallet-reward-reward-detail-modal': RewardDetailModal,
+  'wallet-budget-configuration-drawer': BudgetConfigurationDrawer,
   'wallet-reward-timezone-selectbox': TimeZoneSelectBox,
-  'wallet-reward-configuration-tab': ConfigurationTab,
-  'wallet-reward-send-rewards-tab': SendRewardsTab,
-  'wallet-reward-add-team-form': TeamForm,
-  'wallet-reward-teams-list-tab': TeamsListTab,
 };
 
 for (const key in components) {
