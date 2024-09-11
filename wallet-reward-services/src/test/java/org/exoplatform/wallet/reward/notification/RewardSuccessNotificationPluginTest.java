322860742259841d4d5ef2010ae25a0a0257d9a5
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-09-11 16:09:12 +0200

    feat: Suggest to create a rewarding  budget - MEED-7379 - Meeds-io/MIPs#154 (#554)
    Ability for rewarding admin to create/update reward budget

--- wallet-reward-services/src/test/java/org/exoplatform/wallet/reward/notification/RewardSuccessNotificationPluginTest.java
+++ wallet-reward-services/src/test/java/org/exoplatform/wallet/reward/notification/RewardSuccessNotificationPluginTest.java
@@ -1,18 +1,20 @@
 /*
  * This file is part of the Meeds project (https://meeds.io/).
- * Copyright (C) 2020 Meeds Association
- * contact@meeds.io
+ *
+ * Copyright (C) 2020 - 2024 Meeds Lab contact@meedslab.com
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exoplatform.wallet.reward.notification;
 
@@ -22,38 +24,53 @@ import static org.exoplatform.wallet.utils.RewardUtils.REWARD_REPORT_NOTIFICATIO
 import static org.exoplatform.wallet.utils.RewardUtils.REWARD_SUCCESS_COUNT;
 import static org.exoplatform.wallet.utils.RewardUtils.REWARD_TRANSACTION_COUNT;
 import static org.exoplatform.wallet.utils.RewardUtils.REWARD_VALID_MEMBERS_COUNT;
-import static org.exoplatform.wallet.utils.RewardUtils.getRewardSettings;
 import static org.exoplatform.wallet.utils.WalletUtils.AMOUNT;
+import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Test;
+import org.exoplatform.wallet.model.reward.RewardSettings;
+import org.exoplatform.wallet.model.settings.GlobalSettings;
+import org.exoplatform.wallet.reward.BaseRewardTest;
+import org.exoplatform.wallet.service.WalletService;
+import org.junit.jupiter.api.Test;
 
 import org.exoplatform.commons.api.notification.NotificationContext;
 import org.exoplatform.commons.api.notification.model.NotificationInfo;
-import org.exoplatform.commons.api.notification.plugin.config.PluginConfig;
 import org.exoplatform.commons.notification.impl.NotificationContextImpl;
 import org.exoplatform.container.xml.InitParams;
-import org.exoplatform.container.xml.ObjectParam;
 import org.exoplatform.wallet.model.reward.RewardPeriod;
 import org.exoplatform.wallet.model.reward.RewardReport;
 import org.exoplatform.wallet.model.reward.WalletReward;
 import org.exoplatform.wallet.model.transaction.TransactionDetail;
-import org.exoplatform.wallet.reward.BaseWalletRewardTest;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-public class RewardSuccessNotificationPluginTest extends BaseWalletRewardTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-  /**
-   * Check that provider returns templates correctly
-   */
+@SpringJUnitConfig(BaseRewardTest.class)
+public class RewardSuccessNotificationPluginTest extends BaseRewardTest {
+
+  @Mock
+  private InitParams    initParams;
+
+  @Mock
+  private WalletService walletService;
+
   @Test
   public void testMakeMessage() {
-    RewardSuccessNotificationPlugin plugin = new RewardSuccessNotificationPlugin(getParams());
+    GlobalSettings globalSettings = new GlobalSettings();
+    when(walletService.getSettings()).thenReturn(globalSettings);
+    RewardSuccessNotificationPlugin plugin = new RewardSuccessNotificationPlugin(initParams);
+    plugin.walletService = walletService;
     NotificationContext ctx = NotificationContextImpl.cloneInstance();
 
     RewardReport rewardReport = new RewardReport();
-    RewardPeriod rewardPeriod = RewardPeriod.getCurrentPeriod(getRewardSettings());
+    RewardSettings rewardSettings = new RewardSettings();
+    RewardPeriod rewardPeriod = RewardPeriod.getCurrentPeriod(rewardSettings);
     rewardReport.setPeriod(rewardPeriod);
 
     Set<WalletReward> rewards = new HashSet<>();
@@ -83,19 +100,4 @@ public class RewardSuccessNotificationPluginTest extends BaseWalletRewardTest {
                  notification.getValueOwnerParameter(REWARD_VALID_MEMBERS_COUNT));
     assertEquals(String.valueOf(rewardReport.getTokensSent()), notification.getValueOwnerParameter(AMOUNT));
   }
-
-  private InitParams getParams() {
-    InitParams initParams = new InitParams();
-    ObjectParam objectParam = new ObjectParam();
-    objectParam.setName("plugin-config");
-    objectParam.setType(PluginConfig.class.getName());
-    objectParam.addProperty("pluginId", "RewardSuccessNotificationPlugin");
-    objectParam.addProperty("resourceBundleKey", "UINotification.label.RewardSuccessNotificationPlugin");
-    objectParam.addProperty("order", "1");
-    objectParam.addProperty("groupId", "wallet");
-    objectParam.addProperty("bundlePath", "locale.notification.WalletNotification");
-    initParams.addParam(objectParam);
-    return initParams;
-  }
-
 }
