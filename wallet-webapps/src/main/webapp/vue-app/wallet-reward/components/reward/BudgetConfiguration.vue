01b0b98f3c8bb917c5236786876dd9876123f740
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-09-11 16:09:12 +0200

    feat: Suggest to create a rewarding  budget - MEED-7379 - Meeds-io/MIPs#154 (#554)
    Ability for rewarding admin to create/update reward budget

--- wallet-webapps/src/main/webapp/vue-app/wallet-reward/components/reward/BudgetConfiguration.vue
+++ wallet-webapps/src/main/webapp/vue-app/wallet-reward/components/reward/BudgetConfiguration.vue
@@ -20,24 +20,174 @@
 -->
 <template>
   <v-card
+    :loading="loading"
     class="border-radius border-color my-5 me-5"
     width="60%"
     flat>
     <div class="d-flex flex-column flex-grow-1">
-      <div class="text-header text-wrap px-4 pb-0 pt-3 mt-auto mb-2">
-        {{ $t('wallet.administration.budgetConfiguration') }}
-      </div>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-header text-wrap">
+            {{ $t('wallet.administration.budgetConfiguration') }}
+          </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action v-if="!loading && hasConfiguredBudget" class="ma-auto">
+          <v-tooltip
+            :disabled="$root.isMobile"
+            bottom>
+            <template #activator="{ on }">
+              <div v-on="on">
+                <v-btn
+                  :aria-label="$t('wallet.administration.budgetConfiguration.editConfiguration')"
+                  target="_blank"
+                  small
+                  icon
+                  @click="openConfigurationDrawer">
+                  <v-icon size="18">fas fa-edit</v-icon>
+                </v-btn>
+              </div>
+            </template>
+            <span>{{ $t('wallet.administration.budgetConfiguration.editConfiguration') }}</span>
+          </v-tooltip>
+        </v-list-item-action>
+      </v-list-item>
       <v-spacer />
-      <div class="d-flex flex-column align-center justify-center full-width full-height pt-5">
-        <v-icon color="orange darken-2" size="50">fas fa-coins</v-icon>
-        <span class="mt-5">{{ $t('wallet.administration.budgetConfiguration.setUp') }}</span>
-      </div>
-      <v-btn
-        class="btn btn-primary align-self-center my-4 px-2">
-        <span class="mx-2">
-          {{ $t('wallet.administration.budgetConfiguration.setBudget') }}
-        </span>
-      </v-btn>
+      <template v-if="!loading && hasConfiguredBudget">
+        <v-list-item dense>
+          <v-list-item-content>
+            <v-list-item-title class="text-wrap">
+              {{ $t('wallet.administration.budgetConfiguration.periodType.title') }}
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action class="ma-auto font-weight-bold pe-1">
+            {{ periodTypeLabel }}
+          </v-list-item-action>
+        </v-list-item>
+        <v-list-item dense>
+          <v-list-item-content>
+            <v-list-item-title class="text-wrap">
+              {{ $t('wallet.administration.budgetConfiguration.timeZone.title') }}
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action class="ma-auto font-weight-bold pe-1">
+            {{ periodDatesDisplay }}
+          </v-list-item-action>
+        </v-list-item>
+        <v-list-item dense>
+          <v-list-item-content>
+            <v-list-item-title class="text-wrap">
+              {{ $t('wallet.administration.budgetConfiguration.threshold.title') }}
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action class="ma-auto font-weight-bold pe-1">
+            {{ threshold }} {{ $t('wallet.administration.budgetConfiguration.points') }}
+          </v-list-item-action>
+        </v-list-item>
+        <v-list-item dense>
+          <v-list-item-content>
+            <v-list-item-title class="text-wrap">
+              {{ budgetTypeLabel }}
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action class="ma-auto font-weight-bold pe-1">
+            {{ amount }}  MEED
+          </v-list-item-action>
+        </v-list-item>
+      </template>
+      <template v-else-if="!loading">
+        <div class="d-flex flex-column align-center justify-center full-width full-height pt-5">
+          <v-icon color="orange darken-2" size="50">fas fa-coins</v-icon>
+          <span class="mt-5">{{ $t('wallet.administration.budgetConfiguration.setUp') }}</span>
+        </div>
+        <v-btn
+          class="btn btn-primary align-self-center my-4 px-2"
+          @click="openConfigurationDrawer">
+          <span class="mx-2">
+            {{ $t('wallet.administration.budgetConfiguration.setBudget') }}
+          </span>
+        </v-btn>
+      </template>
     </div>
   </v-card>
 </template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    rewardSettings: {
+      type: Object,
+      default: null
+    },
+    rewardReport: {
+      type: Object,
+      default: null
+    }
+  },
+  data: () => ({
+
+  }),
+  computed: {
+    periodType() {
+      return this.rewardSettings?.periodType;
+    },
+    periodTypeLabel() {
+      return this.$t(`wallet.administration.periodType.label.${this.periodType?.toLowerCase()}`) || '';
+    },
+    rewardPeriod() {
+      return this.rewardReport?.period;
+    },
+    periodDatesDisplay() {
+      if (!this.rewardPeriod) {
+        return '';
+      }
+      const startDateFormatted = this.$dateUtil.formatDateObjectToDisplay(new Date(this.rewardPeriod.startDateInSeconds * 1000), this.dateformat, eXo.env.portal.language);
+      const endDateFormatted = this.$dateUtil.formatDateObjectToDisplay(new Date(this.rewardPeriod.endDateInSeconds * 1000 - 1), this.dateformat, eXo.env.portal.language);
+      return `${startDateFormatted} ${this.$t('exoplatform.wallet.label.to')} ${endDateFormatted}`;
+    },
+    dateformat() {
+      return this.timeZone && {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZoneName: 'short',
+        timeZone: this.timeZone,
+      } || {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      };
+    },
+    timeZone() {
+      return this.rewardPeriod?.timeZone || this.rewardSettings?.timeZone;
+    },
+    amount() {
+      return this.rewardSettings?.amount;
+    },
+    threshold() {
+      return this.rewardSettings?.threshold;
+    },
+    budgetType() {
+      return this.rewardSettings?.budgetType;
+    },
+    budgetTypeLabel() {
+      return this.budgetType === 'FIXED' ? this.$t('wallet.administration.budgetConfiguration.budgetFixed.title') : this.$t('wallet.administration.budgetConfiguration.budgetPerContributor.title');
+    },
+    hasConfiguredBudget() {
+      return this.rewardSettings?.storedSetting;
+    },
+  },
+  methods: {
+    openConfigurationDrawer() {
+      this.$emit('openConfiguration');
+    },
+  },
+};
+</script>
