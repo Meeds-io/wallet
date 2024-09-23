8a835ca3448adba0df9450f23872de21621b17fc
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-09-23 13:24:29 +0200

    feat: List Reward cards per period - MEED-7455 - Meeds-io/MIPs#154 (#559)
    This PR will list the reward report card per period.

---
+++ wallet-api/src/main/java/io/meeds/wallet/wallet/model/reward/RewardTeam.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * Copyright (C) 2020 Meeds Association
+ * contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package io.meeds.wallet.wallet.model.reward;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode.Exclude;
+import lombok.ToString;
+
+@Data
+@ToString
+public class RewardTeam implements Serializable {
+
+  private static final long      serialVersionUID = 4475704534821391132L;
+
+  private Long                   id;
+
+  @Exclude
+  private String                 name;
+
+  @Exclude
+  private String                 description;
+
+  @Exclude
+  private RewardBudgetType       rewardType;
+
+  @Exclude
+  private Double                 budget;
+
+  @Exclude
+  private Long                   spaceId;
+
+  @Exclude
+  private String                 spacePrettyName;
+
+  @Exclude
+  private boolean                disabled;
+
+  @Exclude
+  private boolean                deleted;
+
+  @Exclude
+  private RewardTeamMember       manager;
+
+  @Exclude
+  private List<RewardTeamMember> members;
+
+}
---
+++ wallet-reward-services/src/main/java/io/meeds/wallet/reward/notification/RewardSuccessTemplateProvider.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * Copyright (C) 2020 Meeds Association
+ * contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package io.meeds.wallet.reward.notification;
+
+import static io.meeds.wallet.wallet.utils.RewardUtils.REWARD_SUCCESS_NOTIFICATION_ID;
+
+import java.util.Collections;
+import java.util.Map;
+
+import io.meeds.wallet.reward.service.RewardSettingsService;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.codec.binary.StringUtils;
+
+import org.exoplatform.commons.api.notification.channel.template.TemplateProvider;
+import org.exoplatform.commons.api.notification.model.ChannelKey;
+import org.exoplatform.commons.api.notification.model.PluginKey;
+import org.exoplatform.container.xml.InitParams;
+
+public class RewardSuccessTemplateProvider extends TemplateProvider {
+
+  private static final PluginKey       PLUGIN_KEY       = PluginKey.key(REWARD_SUCCESS_NOTIFICATION_ID);
+
+  private RewardSuccessTemplateBuilder builder          = null;
+
+  @Setter
+  @Getter
+  private String                       pushTemplatePath = null;
+
+  @Setter
+  @Getter
+  private String                       mailTemplatePath = null;
+
+  public RewardSuccessTemplateProvider(RewardSettingsService rewardSettingsService, InitParams initParams) {
+    super(initParams);
+    builder = new RewardSuccessTemplateBuilder(rewardSettingsService, getChannelKey());
+
+    this.templateBuilders.put(PLUGIN_KEY, builder);
+
+    setMailTemplatePath("war:/conf/wallet/templates/notification/mail/RewardSuccessMailPlugin.gtmpl");
+    setPushTemplatePath("war:/conf/wallet/templates/notification/push/RewardSuccessPushPlugin.gtmpl");
+  }
+
+  @Override
+  public Map<PluginKey, String> getTemplateFilePathConfigs() {
+    ChannelKey channelKey = getChannelKey();
+    if (StringUtils.equals(channelKey.getId(), "MAIL_CHANNEL")) {
+      return Collections.singletonMap(PLUGIN_KEY, mailTemplatePath);
+    } else if (StringUtils.equals(channelKey.getId(), "PUSH_CHANNEL")) {
+      return Collections.singletonMap(PLUGIN_KEY, pushTemplatePath);
+    }
+    return Collections.emptyMap();
+  }
+
+  public RewardSuccessTemplateBuilder getBuilder() {
+    return builder;
+  }
+}
