00507f8df09e6b7fe488422e223f6d9e2d77657f
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-09-23 13:24:29 +0200

    feat: List Reward cards per period - MEED-7455 - Meeds-io/MIPs#154 (#559)
    This PR will list the reward report card per period.

--- wallet-webapps/src/main/webapp/vue-app/wallet-reward/initComponents.js
+++ wallet-webapps/src/main/webapp/vue-app/wallet-reward/initComponents.js
@@ -1,19 +1,27 @@
 import RewardApp from './components/RewardApp.vue';
 import RewardManagement from './components/reward/RewardManagement.vue';
+import RewardCard from './components/reward/RewardCard.vue';
+import RewardDetails from './components/reward/RewardDetails.vue';
+import RewardDetailsItem from './components/reward/RewardDetailsItem.vue';
 import CurrentBalance from './components/reward/CurrentBalance.vue';
 import BudgetConfiguration from './components/reward/BudgetConfiguration.vue';
 import BudgetConfigurationDrawer from './components/reward/BudgetConfigurationDrawer.vue';
 import DistributionForecast from './components/reward/DistributionForecast.vue';
 import TimeZoneSelectBox from './components/reward/TimeZoneSelectBox.vue';
+import RewardCardMaskContent from './components/reward/RewardCardMaskContent.vue';
 
 const components = {
   'wallet-reward-app': RewardApp,
   'wallet-reward-management': RewardManagement,
+  'wallet-reward-card': RewardCard,
+  'wallet-reward-details': RewardDetails,
+  'wallet-reward-details-item': RewardDetailsItem,
   'wallet-current-balance': CurrentBalance,
   'wallet-budget-configuration': BudgetConfiguration,
   'wallet-budget-configuration-drawer': BudgetConfigurationDrawer,
   'wallet-budget-distribution-forecast': DistributionForecast,
   'wallet-reward-timezone-selectbox': TimeZoneSelectBox,
+  'wallet-reward-card-mask-content': RewardCardMaskContent,
 };
 
 for (const key in components) {
