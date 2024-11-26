1b5491123c996d32cec53ea410717839f107a902
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-11-26 14:55:40 +0100

    fix: Actions cancelled/deleted must not be rewarded - MEED-7841 - Meeds-io/MIPs#154 (#632)
    Before this change, there were two issues:
    
    The reward items list was not updated when the user reached 0 points
    after the reviewer canceled their achievements for the current period.
    
    The reward items list was not updated at all after the reviewer canceled
    their achievements for a past unrewarded period.

--- wallet-reward-services/src/test/java/io/meeds/wallet/reward/rest/TestRewardReportREST.java
+++ wallet-reward-services/src/test/java/io/meeds/wallet/reward/rest/TestRewardReportREST.java
@@ -415,7 +415,7 @@ class TestRewardReportREST {
   }
 
   private WalletReward walletReward() {
-    return new WalletReward(new Wallet(), new TransactionDetail(), 1L, 100.0, 40.0, rewardPeriod(), 1);
+    return new WalletReward(1, new Wallet(), new TransactionDetail(), 1L, 100.0, 40.0, rewardPeriod(), 1);
   }
 
   @SneakyThrows
