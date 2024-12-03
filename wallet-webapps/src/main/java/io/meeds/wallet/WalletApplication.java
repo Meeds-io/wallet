699761428948b7ce1d9fd702e9693f2d712f4861
Azmi Touil <atouil@exoplatform.com>
2024-12-03 13:59:00 +0100

    feat: Suggest to create a rewarding  budget - MEED-7379 - Meeds-io/MIPs#154 (#554)
    Ability for rewarding admin to create/update reward budget

---
+++ wallet-webapps/src/main/java/io/meeds/wallet/WalletApplication.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+package io.meeds.wallet;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+import io.meeds.spring.AvailableIntegration;
+import io.meeds.spring.kernel.PortalApplicationContextInitializer;
+
+@SpringBootApplication(scanBasePackages = { 
+        WalletApplication.MODULE_NAME,
+        AvailableIntegration.KERNEL_MODULE,
+        AvailableIntegration.JPA_MODULE, 
+        AvailableIntegration.WEB_MODULE, 
+}, 
+exclude = {
+        LiquibaseAutoConfiguration.class,
+})
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application-common.properties")
+public class WalletApplication extends PortalApplicationContextInitializer {
+
+  public static final String MODULE_NAME = "io.meeds.wallet";
+
+}
