98696ec0b4f807992f073217143b87d4cae9179e
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-05-22 16:21:56 +0100

    feat: Improve Wallet Welcome Message UX - MEED-9143 - Meeds-io/meeds#3335 This change will enhance UX of Wallet Welcome Screen in order to align with other Apps UX.

--- wallet-webapps/src/main/webapp/vue-app/wallet-app/initComponents.js
+++ wallet-webapps/src/main/webapp/vue-app/wallet-app/initComponents.js
@@ -3,7 +3,6 @@ import Summary from './components/wallet-app/Summary.vue';
 import SummaryBalance from './components/wallet-app/SummaryBalance.vue';
 import SummaryButtons from './components/wallet-app/SummaryButtons.vue';
 import SummaryTransaction from './components/wallet-app/SummaryTransaction.vue';
-import ToolbarMenu from './components/wallet-app/ToolbarMenu.vue';
 import TransactionHistoryChart from './components/wallet-app/TransactionHistoryChart.vue';
 import TransactionHistoryChartSummary from './components/wallet-app/TransactionHistoryChartSummary.vue';
 import PopoverWalletButton from './components/wallet-app/PopoverWalletButton.vue';
@@ -16,7 +15,6 @@ const components = {
   'wallet-reward-summary-balance': SummaryBalance,
   'wallet-reward-summary-buttons': SummaryButtons,
   'wallet-reward-summary-transaction': SummaryTransaction,
-  'wallet-reward-toolbar-menu': ToolbarMenu,
   'wallet-reward-transaction-history-chart': TransactionHistoryChart,
   'wallet-reward-transaction-history-chart-summary': TransactionHistoryChartSummary,
   'popover-wallet-button': PopoverWalletButton,
