9816e1bed12debf48ff6b858096b9379cf077fb5
Ayoub Zayati <azayati@exoplatform.com>
2026-09-11 18:09:38 +0200

    feat: Digest mail notifications - eXIP7.3.0.22 - integration into feature/mips (#701)

--- wallet-services/src/main/java/io/meeds/wallet/notification/builder/RequestFundsTemplateBuilder.java
+++ wallet-services/src/main/java/io/meeds/wallet/notification/builder/RequestFundsTemplateBuilder.java
@@ -18,7 +18,6 @@ package io.meeds.wallet.notification.builder;
 
 import static io.meeds.wallet.utils.WalletUtils.*;
 
-import java.io.Writer;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -133,11 +132,6 @@ public class RequestFundsTemplateBuilder extends AbstractTemplateBuilder {
     } catch (Exception e) {
       templateContext.put("LAST_UPDATED_TIME", "");
     }
-  }
-
-  @Override
-  protected boolean makeDigest(NotificationContext ctx, Writer writer) {
-    return false;
   }
 
   private void addMessageSubject(MessageInfo messageInfo, TemplateContext templateContext, String type) {
